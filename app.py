f0f361cf2623ff576851810901d8321b5ca8ccaa
cilllilly <moore.cillian@gmail.com>
2024-10-12 11:55:29 -0400

    Started website design

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask 
+from flask_cors import CORS
+from views import views 
+
+app = Flask(__name__, template_folder = "templates",static_folder = "static")
+app.register_blueprint(views, url_prefix = "/views")
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8000)
