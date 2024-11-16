e43dd33fa0e7cb339c6254203b69d689b6384088
cilllilly <moore.cillian@gmail.com>
2024-11-15 19:02:32 -0500

    gemini trial and error

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask 
 from flask_cors import CORS
 from views import views 
+import os
 
 app = Flask(__name__, template_folder = "templates",static_folder = "static")
 app.register_blueprint(views, url_prefix = "/views")
+
 
 if __name__ == "__main__":
     app.run(debug = True, port = 8000)
--- views.py
+++ views.py
@@ -1,14 +1,33 @@
 from flask import render_template, redirect, url_for, Blueprint, request
+import os, pathlib, textwrap
+import google.generativeai as gemini
+from IPython.display import display
+from IPython.display import Markdown
 
 views = Blueprint(__name__, "views")
+geminiAPI = os.environ.get['GEMINI_API']
+print(geminiAPI)
+gemini.configure(api_key= geminiAPI)
+model = gemini.GenerativeModel("models/gemini-1.5-flash")
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 @views.route("/")
 def main():
+    #list of gemini models models/gemini-1.5-flash
+    for m in gemini.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
     return render_template("index.html")
 
 
 @views.route("/calculate", methods = ["POST", "GET"])
 def calculate():
+    response = model.generate_content("How are you today?")
+    #print(to_markdown(response.text))
+    print(response.resolve())
     if request.method == "POST":
         data = request.get_json()
         print(data)
