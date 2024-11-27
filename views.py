e2c1ad6e82e5e57c417b28b53ea602fc2efea554
cilllilly <moore.cillian@gmail.com>
2024-11-27 18:00:05 -0500

    AI and formula

--- views.py
+++ views.py
@@ -1,18 +1,18 @@
 from flask import render_template, redirect, url_for, Blueprint, request
-import os, pathlib, textwrap
+import os, pathlib, textwrap, json, re
 import google.generativeai as gemini
 from IPython.display import display
 from IPython.display import Markdown
+from dotenv import load_dotenv
+load_dotenv()
 
 views = Blueprint(__name__, "views")
-geminiAPI = os.environ.get['GEMINI_API']
+geminiAPI = os.getenv('GEMINI_API')
 print(geminiAPI)
 gemini.configure(api_key= geminiAPI)
 model = gemini.GenerativeModel("models/gemini-1.5-flash")
 
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
 
 @views.route("/")
 def main():
@@ -25,10 +25,22 @@ def main():
 
 @views.route("/calculate", methods = ["POST", "GET"])
 def calculate():
-    response = model.generate_content("How are you today?")
-    #print(to_markdown(response.text))
-    print(response.resolve())
+    
+    
     if request.method == "POST":
         data = request.get_json()
         print(data)
+        weight = round(int(data["weight"]) / 2.2)
+        print(weight)
+        response = model.generate_content(f"How long is the surgery for {data["opperation"]}? Only give us the approximate time in minutes.")
+        surgeryTime = response.text
+        times = re.findall(r'\d+', surgeryTime)
+        times = list(map(int, times))
+        print(surgeryTime)
+        print(times)
+        minDose = (weight * 2) * (times[0]/5)
+        maxDose = (weight * 2) * (times[1]/5)
+        print(minDose, "-", maxDose, "mg")
     return render_template("main.html")
+
+#(lbs/2.2 * 2.0mg) * (time(mins)/5)
